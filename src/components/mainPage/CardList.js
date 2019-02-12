f724f74316440fc21708b7693db408a9a9dec956
Denis Bogatyev <denis3509@gmail.com>
2019-02-12 17:33:03 +0300

    switch to webstorm

---
+++ src/components/mainPage/CardList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+class CardList extends Component {
+    constructor(state){
+        super(state);
+    }
+}
+export default CardList
---
+++ src/components/mainPage/MainPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+
+class MainPage extends Component {
+    constructor(state) {
+        super(state);
+    }
+
+    render() {
+        return (
+            <Grid container>
+                <Grid item xs={3}>
+                </Grid>
+                <Grid item xs={9}>
+                </Grid>
+            </Grid>
+        )
+    }
+}
